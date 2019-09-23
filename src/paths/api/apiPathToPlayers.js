3c5c0dfdcb2dae07fbe3c3f87260d715dca65674
TJ Sornet <tsornet@gmail.com>
2019-09-22 17:47:03 -0700

    Updates

--- src/paths/api/apiPathToPlayers.js
+++
@@ -1,5 +0,0 @@
-
-
-export const apiPathToGetPlayerSeasonGamelogs = ({ season, format }) => {
-    return `https://api.mysportsfeeds.com/v1.2/pull/nfl/${season}/cumulative_player_stats.json?${format}`
-}
--- src/paths/api/apiPathToTeams.js
+++
@@ -1,3 +0,0 @@
-export const apiPathToTeamPlayerStats = ({ season, team }) => {
-  return `https://api.mysportsfeeds.com/v1.2/pull/nfl/${season}/cumulative_player_stats.json?team=${team}`;
-}
